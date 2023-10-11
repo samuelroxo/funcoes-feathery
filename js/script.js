c8328df8bb22563b629a9efb30a6967c96eacea8
Samuel <samuel.roxo@gmail.com>
2023-10-11 15:24:14 -0300

    Add event listener apenas quando elemento existe

--- js/script.js
+++ js/script.js
@@ -20,19 +20,31 @@ let funcOnLoad = function(context) {
 };
 
 let funcOnChange = function(context) {
-	document.getElementById("cep_pre").addEventListener('blur', (e) => {
-		if (!/^[0-9]{5}-[0-9]{3}$/.test(context.fields.cep_pre.value)) {
-			context.fields.cep_pre.setError('CEP incompleto');
-		}
-	});
-	document.getElementById("tel_principal").addEventListener('blur', (e) => {
-		if (!/\((\d{2});\) \d{4,5}-\d{4}/.test(context.fields.tel_principal.value)) {
-			context.fields.tel_principal.setError('Celular incompleto');
-		}
-	});
-	document.getElementById("cpf_do_lead").addEventListener('blur', (e) => {
-		if (!CPF.validate(context.fields.cpf_do_lead.value)) {
-			context.fields.cpf_do_lead.setError('CPF Inválido');
-		}
-	});
+	let cep_pre = document.getElementById("cep_pre"),
+	tel_principal = document.getElementById("tel_principal"),
+	cpf_do_lead = document.getElementById("cpf_do_lead");
+
+	if(cep_pre){
+		cep_pre.addEventListener('blur', (e) => {
+			if (!/^[0-9]{5}-[0-9]{3}$/.test(context.fields.cep_pre.value)) {
+				context.fields.cep_pre.setError('CEP incompleto');
+			}
+		});
+	}
+
+	if(tel_principal){
+		tel_principal.addEventListener('blur', (e) => {
+			if (!/\((\d{2})\) \d{4,5}-\d{4}/.test(context.fields.tel_principal.value)) {
+				context.fields.tel_principal.setError('Celular incompleto');
+			}
+		});
+	}
+
+	if(cpf_do_lead) {
+		cpf_do_lead.addEventListener('blur', (e) => {
+			if (!CPF.validate(context.fields.cpf_do_lead.value)) {
+				context.fields.cpf_do_lead.setError('CPF Inválido');
+			}
+		});
+	}
 };
